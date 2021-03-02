a0b3f0c69737d1cca7c5572af10fcc3f7f13af7d
BjBodner <benjybo7@gmail.com>
2021-03-02 08:56:33 +0200

    made a menu for filling in expenses

--- interface/my_pagame_menu.py
+++ interface/my_pagame_menu.py
@@ -6,6 +6,10 @@ def set_difficulty(value, difficulty):
     print(f"value = {value}, difficulty = {difficulty}")
 
 
+def process_name(value):
+    print(value)
+
+
 def start_the_game():
     print("rock and roll")
 
@@ -17,11 +21,14 @@ if __name__ == "__main__":
     menu = pygame_menu.Menu(300, 400, "Welcome", theme=pygame_menu.themes.THEME_BLUE)
 
     # add all widgets
-    menu.add_text_input("name :", default="John Doe")
+    menu.add_text_input("name :", default="John Doe", onreturn=process_name)
+    menu.add_text_input("price :", default="0", onreturn=process_name, valid_chars=["0", "1"])
+
     menu.add_selector("difficulty :", [("hard", 1), ("easy", 2)], onchange=set_difficulty)
     # menu.add_color_input("select color", "rgb", "1")
     menu.add_image("interface/ball.jpg", scale=(0.1, 0.1))
-    # menu.add_vertical_margin(80)
+    menu.add_vertical_margin(40)
+    menu.add_label("press below to enter or quit")
     menu.add_button("play", start_the_game)
     menu.add_button("quit", pygame_menu.events.PYGAME_QUIT)
 
---
+++ interface/windows/vacation_menu.py
@@ -0,0 +1,67 @@
+import pygame
+import pygame_menu
+# from functools import part
+
+SURFACE_SIZE = (800, 500)
+MENU_SIZE = (500, 600)
+VALID_FLOAT_CHARS = ["0", "1", "2", "2", "3", "4", "5", "6", "7", "8", "9", "."]
+VALID_INT_CHARS = ["0", "1", "2", "2", "3", "4", "5", "6", "7", "8", "9"]
+DEFAULT_COST_PER_DAY_PER_PERSON = {"weekend": 0.4, "small": 0.4, "big": 0.6}
+FLIGHTS_PER_PERSON = {"weekend": 0, "small": 1, "big": 3}
+
+
+def change_vacation_type(vacation_type_):
+    global vacation_type
+    vacation_type = vacation_type_
+
+def process_name(name: str):
+    expense_dict["name"] = name
+    
+def process_num_kids(num_kids: str):
+    expense_dict["expense_params"]["num_kids"] = int(num_kids) if len(num_kids) > 0 else 0
+
+def process_num_days(num_days: str):
+    expense_dict["expense_params"]["num_days"] = int(num_days) if len(num_days) > 0 else 0
+
+def process_cost_per_day_per_person(cost_per_day_per_person: str):
+    expense_dict["expense_params"]["cost_per_day_per_person"] = float(cost_per_day_per_person) if len(cost_per_day_per_person) > 0 else 0
+
+def process_flights_per_person(flights_per_person: str):
+    expense_dict["expense_params"]["flights_per_person"] = float(flights_per_person) if len(flights_per_person) > 0 else 0
+
+def start_the_game():
+    if expense_dict["name"] == "":
+        print(f"highlight the name button to ask the user to fill it in")
+
+    print(expense_dict)
+    print("add the expense to the config and save it")
+
+
+
+if __name__ == "__main__":
+    pygame.init()
+    surface = pygame.display.set_mode(SURFACE_SIZE)
+
+    menu = pygame_menu.Menu(MENU_SIZE[0], MENU_SIZE[1], "vacation", theme=pygame_menu.themes.THEME_BLUE)
+
+    vacation_number = 1
+    vacation_type = "weekend"
+
+    expense_dict = {"name": "", "expense_params": {}, "expense_type": "FamilyTrip"}
+
+
+    # add all widgets to fill in expense
+    menu.add_text_input("Name : ", default=f"-", onchange=process_name)
+    menu.add_selector("Vacation type : ", [("weekend",), ("small",) , ("big",)], onchange=change_vacation_type)
+    menu.add_text_input("Number of days : ", default="0", onchange=process_num_days, valid_chars=VALID_INT_CHARS)
+    menu.add_text_input("Number of kids : ", default="0", onchange=process_num_kids, valid_chars=VALID_INT_CHARS)
+    menu.add_text_input("Cost per person per day: ", default=DEFAULT_COST_PER_DAY_PER_PERSON[vacation_type], onchange=process_cost_per_day_per_person, valid_chars=VALID_FLOAT_CHARS)
+    menu.add_text_input("Cost of flights per person : ", default=FLIGHTS_PER_PERSON[vacation_type], onchange=process_flights_per_person, valid_chars=VALID_FLOAT_CHARS)
+
+    # other buttons
+    menu.add_vertical_margin(40)
+    menu.add_button("Add Expense", start_the_game)
+    menu.add_button("Back", pygame_menu.events.BACK)
+    menu.add_button("quit", pygame_menu.events.PYGAME_QUIT)
+
+    menu.mainloop(surface)
