775c77e85ec0855c8d6226f166c04cd99614aa39
BjBodner <benjybo7@gmail.com>
2021-03-12 21:11:32 +0200

    removed commented out lines

--- V0/menus.py
+++ V0/menus.py
@@ -40,88 +40,27 @@ class SimulationSettingsMenu:
         self.main_menu_object = main_menu_object
 
         # set defaults
-        # number = 1 # get this from menu constructor
-        # default_name = f"job{number}" # get this as input based on the number of vacations already available
-        # default_base_salary_after_taxes = 18.5
-        # default_probability_of_loosing_job =  0.2
-        # default_percentage_of_base_salary_for_stocks =  0.0
-        # default_bonus_fraction_of_annual_income =  0.0
-        # default_monthly_expenses = 7.
         default_number_of_years_to_simulate = self.global_config["simulation_params"]["num_years"]
         default_monthly_expenses = self.global_config["initial"]["expenses"]["fixed_monthly_expenses"]["expense_params"]["monthly_expense"]
 
-
         self.monthly_expenses = default_monthly_expenses
         self.number_of_years_to_simulate = default_number_of_years_to_simulate
-        # default_invest_extra_earnings =  True
-
-
-        # self.job_dict = {"name": "", "job_params": {}}
-
-        # # fill in initial values - or get from constructor
-        # self.job_dict["name"] = default_name
-        # self.job_dict["job_params"]["base_salary_after_taxes"] = default_base_salary_after_taxes
-        # self.job_dict["job_params"]["probability_of_loosing_job"] = default_probability_of_loosing_job
-        # self.job_dict["job_params"]["percentage_of_base_salary_for_stocks"] = default_percentage_of_base_salary_for_stocks
-        # self.job_dict["job_params"]["bonus_fraction_of_annual_income"] = default_bonus_fraction_of_annual_income
-        # self.job_dict["job_params"]["options_per_year"] = default_options_per_year
-        # self.job_dict["job_params"]["has_pension_plan"] = default_has_pension_plan
-        # self.job_dict["job_params"]["has_keren_hishtalmut_plan"] = default_has_keren_hishtalmut_plan
-
-        # add all widgets to fill in expense
-        # self.menu.add_text_input("Name: ", default=default_name, onchange=self.process_name, font_size=38)
-        # self.name_widget = self.menu.get_widgets()[-1]
 
         self.menu.add_text_input("monthly_expenses : ", default=default_monthly_expenses, onchange=self.process_monthly_expenses, valid_chars=VALID_FLOAT_CHARS, align=pygame_menu.locals.ALIGN_LEFT, 
             font_size=20, font_color=(0,0,0),)
         self.menu.add_text_input("number_of_years_to_simulate : ", default=default_number_of_years_to_simulate, onchange=self.process_number_of_years_to_simulate, valid_chars=VALID_INT_CHARS, align=pygame_menu.locals.ALIGN_LEFT, 
             font_size=20, font_color=(0,0,0),)
 
-
-        # self.menu.add_text_input("base_salary_after_taxes : ", default=default_base_salary_after_taxes, onchange=self.process_base_salary_after_taxes, valid_chars=VALID_FLOAT_CHARS, align=pygame_menu.locals.ALIGN_LEFT, 
-        #     font_size=20, font_color=(0,0,0),)
-        # self.menu.add_text_input("probability_of_loosing_job : ", default=default_probability_of_loosing_job, onchange=self.process_probability_of_loosing_job, valid_chars=VALID_FLOAT_CHARS, align=pygame_menu.locals.ALIGN_LEFT, 
-        #     font_size=20, font_color=(0,0,0),)
-        # self.menu.add_text_input("percentage_of_base_salary_for_stocks : ", default=default_percentage_of_base_salary_for_stocks, onchange=self.process_percentage_of_base_salary_for_stocks, valid_chars=VALID_FLOAT_CHARS, align=pygame_menu.locals.ALIGN_LEFT, 
-        #     font_size=20, font_color=(0,0,0),)
-        # self.menu.add_text_input("bonus_fraction_of_annual_income : ", default=default_bonus_fraction_of_annual_income, onchange=self.process_bonus_fraction_of_annual_income, valid_chars=VALID_FLOAT_CHARS, align=pygame_menu.locals.ALIGN_LEFT, 
-        #     font_size=20, font_color=(0,0,0),)
-        # self.menu.add_text_input("options_per_year : ", default=default_options_per_year, onchange=self.process_options_per_year, valid_chars=VALID_FLOAT_CHARS, align=pygame_menu.locals.ALIGN_LEFT, 
-        #     font_size=20, font_color=(0,0,0),)
-        # self.menu.add_selector("has_pension_plan : ", default=default_has_pension_plan, items=[("True",), ("False",)], onchange=self.process_has_pension_plan, align=pygame_menu.locals.ALIGN_LEFT, 
-        #     font_size=20, font_color=(0,0,0),)
-        # self.menu.add_selector("has_keren_hishtalmut_plan : ", default=default_has_keren_hishtalmut_plan, items=[("True",), ("False",)], onchange=self.process_has_keren_hishtalmut_plan, align=pygame_menu.locals.ALIGN_LEFT, 
-        #     font_size=20, font_color=(0,0,0),)
-
         # other buttons
         self.menu.add_vertical_margin(20)
         self.menu.add_button("Save Settings", self.save_settings, font_size=24, font_color=(0,0,0))
         self.menu.add_button("Back", pygame_menu.events.BACK, font_size=24, font_color=(0,0,0))
 
-        # all local methods
-    # def process_name(self, name: str):
-    #     self.job_dict["name"] = name
-        
     def process_monthly_expenses(self, monthly_expenses: str):
         self.monthly_expenses = float(monthly_expenses) if len(monthly_expenses) > 0 else 0
 
     def process_number_of_years_to_simulate(self, number_of_years_to_simulate: str):
         self.number_of_years_to_simulate = int(number_of_years_to_simulate) if len(number_of_years_to_simulate) > 0 else 0
-
-    # def process_percentage_of_base_salary_for_stocks(self, percentage_of_base_salary_for_stocks: str):
-    #     self.job_dict["job_params"]["percentage_of_base_salary_for_stocks"] = float(percentage_of_base_salary_for_stocks) if len(percentage_of_base_salary_for_stocks) > 0 else 0
-
-    # def process_bonus_fraction_of_annual_income(self, bonus_fraction_of_annual_income: str):
-    #     self.job_dict["job_params"]["probability_of_loosing_job"] = float(bonus_fraction_of_annual_income) if len(bonus_fraction_of_annual_income) > 0 else 0
-
-    # def process_options_per_year(self, options_per_year: str):
-    #     self.job_dict["job_params"]["options_per_year"] = float(options_per_year) if len(options_per_year) > 0 else 0
-
-    # def process_has_pension_plan(self, has_pension_plan):
-    #     self.job_dict["job_params"]["has_pension_plan"] = STR_2_BOOL[has_pension_plan[0][0]]
-
-    # def process_has_keren_hishtalmut_plan(self, has_keren_hishtalmut_plan):
-    #     self.job_dict["job_params"]["has_keren_hishtalmut_plan"] = STR_2_BOOL[has_keren_hishtalmut_plan[0][0]]
 
     def save_settings(self):
         # Add here a function to write the expese dict to the global config
@@ -130,11 +69,6 @@ class SimulationSettingsMenu:
         with open(self.config_file, "r") as fh:
             self.global_config = yaml.load(fh, yaml.FullLoader)
 
-        # add the income to the config of all the saved incomes - the name is the job name
-        # name = self.job_dict["name"]
-
-        # self.global_config["initial"]["expenses"][name] = copy.deepcopy(TEMPLATE_JOB)["template_job"] 
-        # self.global_config["initial"]["expenses"][name]["jobs"]["main_job"] = {**self.job_dict["job_params"]}
         self.global_config["simulation_params"]["num_years"] = self.number_of_years_to_simulate
 
         self.global_config["initial"]["expenses"] = {}
@@ -299,12 +233,9 @@ class CompareMenu:
 
     def __init__(self, name, global_config, config_file):
 
-        # self.num_saved_offers = 10
         self.num_saved_offers = len(global_config["initial"]["incomes"])
         self.all_offers = list(global_config["initial"]["incomes"].keys())
 
-
-        # self.offers_to_compare = {}
         self.name_to_widget = {}
         self.num_rows_per_colum = self.num_saved_offers + 2
 
@@ -324,14 +255,11 @@ class CompareMenu:
     def add_offer_to_compare_list(self, offer_name):
         # adds an offer to the compare list
         if offer_name not in self.name_to_widget.keys():
-            # self.offers_to_compare[]offer_name)
             self.menu.add_button(offer_name, partial(self.remove_offer_from_compare_list, offer_name), align=pygame_menu.locals.ALIGN_RIGHT, 
             font_size=20, font_color=(0,0,0),
-            # widget_id=widget_id
             )
             self.name_to_widget[offer_name] = self.menu.get_widgets()[-1]
 
-            # TODO add the details of the config we selected to a temporary config we will run
             self.temporary_config["initial"]["incomes"][offer_name] = copy.deepcopy(self.global_config["initial"]["incomes"][offer_name])
             self.temporary_config["simulation_params"]["names_of_parents"].append(offer_name)
 
@@ -392,20 +320,10 @@ class RemoveMenu:
 
     def __init__(self, name, global_config, config_file, main_menu_object):
 
-        # self.num_saved_offers = len(global_config["initial"]["incomes"])
-        # self.all_offers = list(global_config["initial"]["incomes"].keys())
-
-
-        # self.offers_to_compare = {}
         self.name_to_widget = {}
-        # self.num_rows_per_colum = self.num_saved_offers + 2
-
         self.global_config = global_config
         self.config_file = config_file
         self.main_menu_object = main_menu_object
-        # self.initialize_temporary_config()
-
-        # self.menu = pygame_menu.Menu(MENU_SIZE[0], MENU_SIZE[1], "Remove Offers", columns=2, rows=self.num_rows_per_colum, theme=pygame_menu.themes.THEME_BLUE)
         self.initialize_remove_offers_menu()
 
     def initialize_temporary_config(self):
@@ -483,7 +401,6 @@ class RemoveMenu:
         # update compare menu
         self.initialize_remove_offers_menu()
         self.reinitialize_main_menu_widgets()
-        # self.initialize_remove_offers_menu()
 
     def reinitialize_main_menu_widgets(self):
         self.menu_name_to_widget = {}
@@ -501,15 +418,13 @@ class MainMenu:
 
         self.config = self.load_config(config_file)
         self.config_file = config_file
-        # self.job_menu = JobMenu("Add Offer", self.config, config_file, self)
-        # self.simulation_settings_menu = SimulationSettingsMenu("Simulation Settings", self.config, config_file, self) # for this just use a fixed expense type
+
         self.create_submenus()
         self.menu_name_to_widget = {}
 
         # set defaults from the constructor
         self.default_name = name if name is not None else f"bills{number}"
 
-        # self.initialize_expense_dict()
         self.add_parameter_buttons_and_fields()
         self.add_navigation_buttons()
 
